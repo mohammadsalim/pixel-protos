ba4c2e7c84700c804f81496bca1a904319f05fbb
Mohammad Salim <jd2474ev@gmail.com>
2021-08-17 22:52:45 -0400

    replaced images and reformatted some hero elements

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -30,8 +30,8 @@ jQuery(document).ready(function() {
         Background slideshow
     */
     $('.top-content').backstretch("assets/img/protopixel.gif");
-    $('.call-to-action-container').backstretch("assets/img/protopixel.gif");
-    $('.testimonials-container').backstretch("assets/img/protopixel.gif");
+    $('.call-to-action-container').backstretch("assets/img/ctrlbanner.png");
+    $('.testimonials-container').backstretch("assets/img/prototype.png");
 
     $('#top-navbar-1').on('shown.bs.collapse', function(){
     	$('.top-content').backstretch("resize");
