3517a3a665019dc2a5620c3b01a9af4639d57077
Mohammad Salim <jd2474ev@gmail.com>
2021-08-18 22:55:44 -0400

    Added form and validation functions. Tiedied up mint button and input

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,6 +11,24 @@ function scroll_to(clicked_link, nav_height) {
 	}
 }
 
+// JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
 
 jQuery(document).ready(function() {
 
